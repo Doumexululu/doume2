e6f2e178f0c37fe158765cbbf924050f71a13b25
Aline3220 <874024374@qq.com>
2017-08-22 19:27:05 +0800

    update

--- js/public.js
+++ js/public.js
@@ -4,4 +4,17 @@ $(function(){
     },function(){
 
     });
+
+    // header
+    $('.download').hover(function(){
+    	$('.download img').show();
+    },function(){
+    	$('.download img').hide();
+    })
+
+    $('.head-img-warrp').hover(function(){
+    	$('.head-img-warrp .head-fixed').show();
+    },function(){
+    	$('.head-img-warrp .head-fixed').hide();
+    })
 })
