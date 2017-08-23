387526bd13b0e22c93150017d92f214bc58a71d4
Aline3220 <874024374@qq.com>
2017-08-23 17:23:48 +0800

    update

--- js/public.js
+++ js/public.js
@@ -5,17 +5,29 @@ $(function(){
 
     });
 
+// 登录
+
+    $(".js-login").click(function(){
+        $(".fix-login").hide();
+        $("#js-login").show();
+    })
+// 注册
+    $(".js-sign").click(function(){
+        $(".fix-login").hide();
+        $("#js-sign").show();
+    })
+
     // header
     $('.download').hover(function(){
-    	$('.download img').show();
+        $('.download img').show();
     },function(){
-    	$('.download img').hide();
+        $('.download img').hide();
     })
 
     $('.head-img-warrp').hover(function(){
-    	$('.head-img-warrp .head-fixed').show();
+        $('.head-img-warrp .head-fixed').show();
     },function(){
-    	$('.head-img-warrp .head-fixed').hide();
+        $('.head-img-warrp .head-fixed').hide();
     })
 
     var tabs = function (tab, con) {
@@ -27,5 +39,6 @@ $(function(){
             con.eq(index).addClass('active');
         })
     };
-    tabs($(".login-head ul span"),$(".login-panel"));
+    tabs($(".login-head ul span"),$("#js-login .login-panel"));
+
 })
